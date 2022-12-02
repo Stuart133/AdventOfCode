d40353a9ff42c9d64ae0bec0a21e2630479c20e7
Stuart Reid <stuart1334@gmail.com>
2022-12-02 09:35:33 +0000

    Day 2 part 1

--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -2,6 +2,58 @@ use std::{fs::File, io::Read, path::Path};
 
 fn main() {
     day_one();
+    day_two();
+}
+
+fn day_two() {
+    let data = read_file_to_string(Path::new("data/2.txt"));
+
+    let mut score = 0;
+    for round in data.split("\n") {
+        let mut r = round.split(" ");
+        let opponent = Move::parse(r.next().unwrap());
+        let us = Move::parse(r.next().unwrap());
+        score += us.play_round(&opponent);
+    }
+
+    println!("{}", score);
+}
+
+enum Move {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl Move {
+    fn parse(raw_move: &str) -> Self {
+        match raw_move {
+            "A" | "X" => Self::Rock,
+            "B" | "Y" => Self::Paper,
+            "C" | "Z" => Self::Scissors,
+            _ => panic!("unexpected move type"),
+        }
+    }
+
+    fn play_round(&self, other: &Move) -> i32 {
+        match self {
+            Move::Rock => match other {
+                Move::Rock => 4,
+                Move::Paper => 1,
+                Move::Scissors => 7,
+            },
+            Move::Paper => match other {
+                Move::Rock => 8,
+                Move::Paper => 5,
+                Move::Scissors => 2,
+            },
+            Move::Scissors => match other {
+                Move::Rock => 3,
+                Move::Paper => 9,
+                Move::Scissors => 6,
+            },
+        }
+    }
 }
 
 fn day_one() {
