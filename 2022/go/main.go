80ecd6a2d72ae93fb2df81709cf271df5b69c3de
Stuart133 <stuart1334@gmail.com>
2022-12-20 12:06:07 +0000

    Day 20

--- 2022/go/main.go
+++ 2022/go/main.go
@@ -8,17 +8,21 @@ import (
 )
 
 func main() {
-	data, err := ioutil.ReadFile("data_smol.txt")
+	data, err := ioutil.ReadFile("data.txt")
 	if err != nil {
-		fmt.Printf("error readnig file: %s", err)
+		fmt.Printf("error readingg file: %s", err)
 		return
 	}
 
+	len := 0
 	order := make([]*listNode, 0)
 	head := &listNode{}
 	current := head
+	key := 811589153
 	for _, line := range strings.Split(string(data), "\n") {
+		len++
 		current.value, err = strconv.Atoi(line)
+		current.value = current.value * key
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -38,53 +42,47 @@ func main() {
 	tail.next = head
 
 	var zero *listNode
-	for _, node := range order {
-		if node.value == 0 {
-			zero = node
-			continue
+	for i := 0; i < 10; i++ {
+		for _, node := range order {
+			if node.value == 0 {
+				zero = node
+				continue
+			}
+
+			new_pos := node.move(node.value % (len - 1))
+
+			if new_pos == node {
+				continue
+			}
+
+			// Splice out old node
+			node.next.prev = node.prev
+			node.prev.next = node.next
+
+			// Splice in the new node
+			if node.value > 0 {
+				temp := new_pos.next
+				new_pos.next = node
+				temp.prev = node
+
+				node.next = temp
+				node.prev = new_pos
+			} else {
+				temp := new_pos.prev
+				new_pos.prev = node
+				temp.next = node
+
+				node.prev = temp
+				node.next = new_pos
+			}
 		}
-
-		if node == head {
-			head = node.next
-		}
-
-		new_pos := node.move(node.value)
-
-		if new_pos == node {
-			fmt.Println("I told you so")
-			continue
-		}
-
-		// Splice out old node
-		node.next.prev = node.prev
-		node.prev.next = node.next
-
-		// Splice in the new node
-		if node.value > 0 {
-			temp := new_pos.next
-			new_pos.next = node
-			temp.prev = node
-
-			node.next = temp
-			node.prev = new_pos
-		} else {
-			temp := new_pos.prev
-			new_pos.prev = node
-			temp.next = node
-
-			node.prev = temp
-			node.next = new_pos
-		}
-
-		// head.print(7)
-		// fmt.Println()
 	}
 
 	one := zero.move(1000)
-	two := one.move(1000)
-	three := two.move(1000)
+	two := zero.move(2000)
+	three := zero.move(3000)
 
-	fmt.Println(one.value + two.value + three.value)
+	fmt.Printf("%d", one.value+two.value+three.value)
 }
 
 type listNode struct {
@@ -106,12 +104,4 @@ func (n *listNode) move(count int) *listNode {
 	}
 
 	return current
-}
-
-func (n *listNode) print(len int) {
-	current := n
-	for i := 0; i < len; i++ {
-		fmt.Print(current.value, " ")
-		current = current.next
-	}
 }
